de7a1b3b511ca219f19d072e076c048ceab85a9f
carsendecker <cad631@nyu.edu>
2019-02-22 14:25:08 -0500

    actually putting my quote? -Carsen

--- ItPfG Class/Assets/Quotes.cs
+++ ItPfG Class/Assets/Quotes.cs
@@ -1,4 +1,4 @@
 ﻿"Tell me your favorite quote" --Misha
 "Teamwork makes the dream work" --Anthony 
 Eleanor: "Always" --- Severus Snape
-
+Carsen: "NullReferenceException: Object reference not set to an instance of an object" -- Unity
