1591492e7cd3c87fc32d6eb73163b3d426999218
LifeVirus <acheang97@gmail.com>
2019-02-22 14:55:28 -0500

    removed Quotes.cs, replaced with Quotes.md

--- ItPfG Class/Assets/Quotes.cs
+++
@@ -1,7 +0,0 @@
-/*
-﻿"Tell me your favorite quote" --Misha
-"Teamwork makes the dream work" --Anthony 
-Eleanor: "Always" --- Severus Snape
-Carsen: "NullReferenceException: Object reference not set to an instance of an object" -- Unity
-
-*/
