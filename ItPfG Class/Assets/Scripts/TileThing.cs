8ef6993728ff18c03f8ffe56084888ddf81dd9b2
Misha Favorov <otspiii@gmail.com>
2019-02-28 22:03:04 -0500

    A new demo game!

---
+++ ItPfG Class/Assets/Scripts/TileThing.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileThing : MonoBehaviour
+{
+    //What world things are in me?
+    public List<WorldThing> Contents;
+    
+    //What are my map coordinates?
+    public int X;
+    public int Y;
+    
+    
+}
---
+++ ItPfG Class/Assets/Scripts/WorldThing.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldThing : MonoBehaviour
+{
+    public TileThing Location;
+    
+    //I put all my Start/Update code in virtual functions so they can be messed with more easily by children
+    void Start()
+    {
+        OnStart();
+    }
+
+    void Update()
+    {
+        OnUpdate();
+    }
+
+    protected virtual void OnStart()
+    {
+
+    }
+
+    protected virtual void OnUpdate()
+    {
+
+    }
+
+    //When I first spawn a world thing I need to run setup on it so that it does basic stuff like place itself
+    public virtual void Setup(TileThing start)
+    {
+        SetLocation(start);
+    }
+
+    //When I'm destroyed make sure I destroy myself safely
+    public virtual void Despawn()
+    {
+        if (Location != null)
+            LeaveTile(Location);
+        Destroy(gameObject);
+    }
+    
+    //This code is responsible for making a WT safely leave its old tile and join its new tile
+    //Tiles keep track of what objects are in them
+    //Also physically move to the tile
+    public void SetLocation(TileThing tile)
+    {
+        if (Location != null)
+            LeaveTile(Location);
+        Location = tile;
+        if (!Location.Contents.Contains(this))
+            Location.Contents.Add(this);
+        transform.SetParent(Location.transform);
+        transform.localPosition = Vector3.zero;
+    }
+
+    //When you leave a tile remove yourself from its contents
+    public void LeaveTile(TileThing tile)
+    {
+        tile.Contents.Remove(this);
+    }
+
+    
+
+}
